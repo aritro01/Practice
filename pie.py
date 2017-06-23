5fb79434c4d14db3cf634f985610d146e88abe5a
Aritro Ghosh <aritro01@users.noreply.github.com>
2017-06-23 18:29:12 +0530

    Add files via upload

---
+++ pie.py
@@ -0,0 +1,14 @@
+print("igkeuwgcue")
+print(1+2+3+4.0+5)
+print("This is how you find square")
+print(2**5)
+print("this is 2^5")
+
+print("this is modulo operator[remainder]")
+print(20%3)
+#floor division discards the fraction part
+print('this is floor division[quotient]')
+print(20//3)
+#whole division with fractional part
+print('this is division[quotient]')
+print(20/3)
